4e660b23d56196d75f3b4f386fbf2c519c11354b
Codby-Sa <sbmaciel8@gmail.com>
2025-11-13 19:36:00 -0400

    feature: ajustes no css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import './styles/theme.css';
 import './styles/global.css';
 import Heading from './components/Heading';
+import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 
 export function App() {
 
   return (
     <>
-      <Heading/>
-      <h1>oi</h1>
+      <Container>
+          <Heading> LOGO </Heading>
+      </Container>
+    
+      <Container>
+          <section> MENU </section>
+      </Container>
+
+      <Container>
+          <section> FORM </section>
+      </Container>
+
+      <Container>
+          <section> FOOTEER </section>
+      </Container>
     </>
   )
 }
--- src/components/Heading.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './Heading.module.css'
-export default function Heading() {
-    return <h1 className={styles.heading}>
-        Heading Component
-        </h1>;
-}
