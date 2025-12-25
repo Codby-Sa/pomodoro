a6673cca620746910af0d2eb6018fc296cf53489
Codby-Sa <sbmaciel8@gmail.com>
2025-12-25 16:37:36 -0400

    feature: light and dark theme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css'
 
 
-export default function Heading() {
+export default function Footer() {
     return <footer className={styles.footer}>
         <a href="">Entenda como funciona a tecnica pomodoro</a>
         <a href="">Chonos pomodoro &copy; {new Date().getFullYear()} - feito com ❤️ </a>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,51 @@
 import styles from './styles.module.css'
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'light' | 'dark';
 
 export default function Menu() {
+
+    const [theme, setTheme] =  useState<AvailableThemes>(() => {
+        const savedTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+        return savedTheme;
+    });
+
+    const nextThemeIcon = {
+        dark: <SunIcon/>,
+        light: <MoonIcon/>,
+    };
+
+
+    function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        event.preventDefault();
+
+        setTheme(
+            prevTheme => {
+            const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return nextTheme;
+        });
+
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
     return (
     <nav className={styles.menu}>
-        <a className={styles.menuLink} href="">
+        <a className={styles.menuLink} href="#" area-label="Home" title='Home'>
             <HouseIcon/>
         </a>
-        <a className={styles.menuLink} href="">
+        <a className={styles.menuLink} href="#" area-label="History" title='History'>
             <HistoryIcon/>
         </a>
-        <a className={styles.menuLink} href="">
+        <a className={styles.menuLink} href="#" area-label="Settings" title='Settings'>
             <SettingsIcon/>
         </a>
-        <a className={styles.menuLink} href="">
-            <SunIcon/>
+        <a className={styles.menuLink} href="#" area-label="Theme" title='Theme' onClick={handleThemeChange}>
+            {nextThemeIcon[theme]}
         </a>
     </nav>
     );
