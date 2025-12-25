cd96f1b9bfcad553faec56f88d5179bd273fac5d
Codby-Sa <sbmaciel8@gmail.com>
2025-12-25 19:52:20 -0400

    aula 41

--- src/App.tsx
+++ src/App.tsx
@@ -4,55 +4,11 @@ import { Container } from './components/Container';
 import Logo from './components/Logo';
 import Menu from './components/Menu';
 import CountDown from './components/CountDown';
-import DeafaultInput from './components/DefaultInput';
-import Cycles from './components/Cycles';
-import DefaultButton from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import Footer from './components/Footer';
+import { MainForm } from './components/MainForm';
+import { Home } from './pages/Home';
 
 export function App() {
 
-  return (
-    <>
-
-      <Container>
-        <Logo/>
-      </Container>
-    
-      <Container>
-        <Menu/>
-      </Container>
-
-      <Container>
-        <CountDown/>
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-
-          <div className='formRow'>
-           <DeafaultInput type='text' id='input' labelText='Título' placeholder='Digite Algo'/>
-          </div>
-
-          <div className='formRow'>
-            <p>Nesse ciclo descanse por 5 minutos</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles/>
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon/>}/>
-          </div>
-
-        </form>
-      </Container>
-
-      <Container>
-        <Footer/>
-      </Container>
-      
-    </>
-  )
+  return <Home />;
 }
