14f23c01c87d7a611859e0be31a75c063579d8b2
Codby-Sa <sbmaciel8@gmail.com>
2026-02-12 23:20:14 -0400

    feat: componentização react-router

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
-import styles from './styles.module.css'
-
+import styles from "./styles.module.css";
+import { RouterLink } from "../RouterLink";
 
 export default function Footer() {
-    return <footer className={styles.footer}>
-        <a href="">Entenda como funciona a tecnica pomodoro</a>
-        <a href="">Chonos pomodoro &copy; {new Date().getFullYear()} - feito com ❤️ </a>
+  return (
+    <footer className={styles.footer}>
+      <RouterLink href="/about-pomodoro">Entenda como funciona a tecnica pomodoro</RouterLink>
+      <RouterLink href="/">
+        Chonos pomodoro &copy; {new Date().getFullYear()} - feito com ❤️{" "}
+      </RouterLink>
     </footer>
-} 
+  );
+}
